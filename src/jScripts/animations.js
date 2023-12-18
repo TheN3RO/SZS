d8c8ae055f243e2dbea71ec3a7c85be6562b6498
TheN3RO <58180782+TheN3RO@users.noreply.github.com>
2023-12-18 20:17:34 +0100

    SZS_v0.1.1-Layout-design
    -Smartphone view fixes

--- src/jScripts/animations.js
+++ src/jScripts/animations.js
@@ -4,8 +4,9 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.utils.toArray('.stageAnimation').forEach((element, index) => {
     const direction = index % 2 === 0 ? 1 : -1; // Alternate direction for each element
 
-    gsap.from(element, {
+    gsap.fromTo(element, {
         x: 100 * direction, // Initial x position (off-screen)
+        display: "none",
         opacity: 0, // Initial opacity
         duration: 1, // Animation duration
         scrollTrigger: {
@@ -13,7 +14,18 @@ gsap.utils.toArray('.stageAnimation').forEach((element, index) => {
             start: 'top bottom', // Trigger when the top of the element reaches the bottom of the viewport
             end: 'top top', // Trigger when the bottom of the element reaches the top of the viewport
             scrub: true, // Smoothly adjust animation as you scroll
-        },
+        }, 
+    }, {
+        x: 0, // Initial x position (off-screen)
+        display: "block",
+        opacity: 1, // Initial opacity
+        duration: 1, // Animation duration
+        scrollTrigger: {
+            trigger: element,
+            start: 'top bottom', // Trigger when the top of the element reaches the bottom of the viewport
+            end: 'top top', // Trigger when the bottom of the element reaches the top of the viewport
+            scrub: true, // Smoothly adjust animation as you scroll
+        }, 
     });
 });
 
@@ -26,21 +38,3 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
-
-//scroll animation
-const info = document.querySelector('#headerInfo');
-const cityLogo = document.querySelector('.cityLogo');
-const districtLogo = document.querySelector('.districtLogo');
-const countyLogo = document.querySelector('.countyLogo');
-window.addEventListener('scroll', () => {
-    let scrollPos = window.scrollY;
-    if (scrollPos > 600) {
-        info.style.display = "none"
-    } else {
-        info.style.paddingTop = scrollPos*2.5+"px";
-        cityLogo.style.right = scrollPos*1.5+"px";
-        districtLogo.style.top = scrollPos*2+"px";
-        countyLogo.style.left = scrollPos*1.5+"px";
-        info.style.display = "block"
-    }
-})
