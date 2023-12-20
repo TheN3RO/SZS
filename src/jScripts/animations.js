5885fd641b83612c98570e2106dbd1e62e21b0bd
TheN3RO <58180782+TheN3RO@users.noreply.github.com>
2023-12-20 22:17:10 +0100

    SZS_v0.2-Layout-design
    Design fixes

--- src/jScripts/animations.js
+++ src/jScripts/animations.js
@@ -1,34 +1,3 @@
-//competition animation
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.utils.toArray('.stageAnimation').forEach((element, index) => {
-    const direction = index % 2 === 0 ? 1 : -1; // Alternate direction for each element
-
-    gsap.fromTo(element, {
-        x: 100 * direction, // Initial x position (off-screen)
-        display: "none",
-        opacity: 0, // Initial opacity
-        duration: 1, // Animation duration
-        scrollTrigger: {
-            trigger: element,
-            start: 'top bottom', // Trigger when the top of the element reaches the bottom of the viewport
-            end: 'top top', // Trigger when the bottom of the element reaches the top of the viewport
-            scrub: true, // Smoothly adjust animation as you scroll
-        }, 
-    }, {
-        x: 0, // Initial x position (off-screen)
-        display: "block",
-        opacity: 1, // Initial opacity
-        duration: 1, // Animation duration
-        scrollTrigger: {
-            trigger: element,
-            start: 'top bottom', // Trigger when the top of the element reaches the bottom of the viewport
-            end: 'top top', // Trigger when the bottom of the element reaches the top of the viewport
-            scrub: true, // Smoothly adjust animation as you scroll
-        }, 
-    });
-});
-
 //scroll smooth
 const lenis = new Lenis()
 
